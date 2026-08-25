607dd42925ff399aeb5c1966cbfb5acec4a25b6d
TikaFlow <tika_flow@qq.com>
2026-08-26 02:05:17 +0800

    refactor(eventloop): 为任务执行添加安全调用钩子
    统一微任务、宏任务和时间线任务的异常捕获逻辑，通过safeCall统一处理panic

--- eventloop.go
+++ eventloop.go
@@ -28,13 +28,13 @@ type EventLoop struct {
 // 遍历 el.timeline.tasks 是无锁读，因为此时所有调度都停止，可安全访问
 func (el *EventLoop) flushTasks() {
 	for task := range el.microtaskQueue.Pop() {
-		task()
+		el.hooks.safeCall(PromisePanic, task)
 	}
 	for task := range el.macrotaskQueue.Pop() {
-		task()
+		el.hooks.safeCall(TimerPanic, task)
 	}
 	for _, task := range el.timeline.tasks {
-		task.callback()
+		el.hooks.safeCall(TimerPanic, task.callback)
 	}
 }
 
